2a1d11012aaf77dd8204a5920fa640d7e8c53b5d
Ismaila DIALLO <idiallo@assurone-group.com>
2020-10-13 12:23:33 +0200

    Ajout dashboard et paramétrer les routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,8 +10,6 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
@@ -20,24 +18,9 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  /*getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
-  }*/
-
-  
-  /*hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };*/
 
 }
